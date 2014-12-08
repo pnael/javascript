0615b05fb5b825676274690351f98c4510109ce5
Philippe Naël <philippe.nael@gmail.com>
2014-12-08 21:04:20 +0100

    Ajout de la gestion d'erreur

--- asyncio.js
+++ asyncio.js
@@ -3,6 +3,7 @@ var myNumber = undefined;
 
 function addOne(callback) {
 	fs.readFile('number.txt', function doneReading(err,fileContents) {
+		if (err) return console.log("Can't open number.txt");
 		myNumber = parseInt(fileContents);
 		myNumber++;
 		callback();
